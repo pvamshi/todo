cc1262e5a3d1185e35b52e0c0b01734442ac1174
vamshi <pvamshi@gmail.com>
2015-04-17 02:20:30 -0700

    more changes , should be working hopefully

--- src/main/java/com/skeptors/Constants.java
+++ src/main/java/com/skeptors/Constants.java
@@ -4,8 +4,12 @@ package com.skeptors;
  * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "318868172588-25cmbhejflvdc95ku4b3okg4a45akrhs.apps.googleusercontent.com";
-//  public static final String WEB_CLIENT_ID = "318868172588-j129600gmr6ebjtqcvpldta0uocnnven.apps.googleusercontent.com";
+  //prod
+//  public static final String WEB_CLIENT_ID = "318868172588-25cmbhejflvdc95ku4b3okg4a45akrhs.apps.googleusercontent.com";
+  //localhost:8080
+  //public static final String WEB_CLIENT_ID = "318868172588-j129600gmr6ebjtqcvpldta0uocnnven.apps.googleusercontent.com";
+//127.0.0.1:8080
+  public static final String WEB_CLIENT_ID = "318868172588-aleo8u1fki0h8j04v7oql71avlc8vj7s.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
